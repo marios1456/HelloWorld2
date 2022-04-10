3cb98f88a550c6d11e037ec8571e6589ef48ed04
Marios <71929364+marios1456@users.noreply.github.com>
2022-04-10 13:10:27 +0200

    Update Car.groovy

--- src/com/acceleratedskillup/Car.groovy
+++ src/com/acceleratedskillup/Car.groovy
@@ -4,13 +4,13 @@ class Car {
   int numberOfDoors
   String model
   def script
-  Car(script) {
-    this.script = script
-  }
-  
-  void printOutCar() {
-    script.echo "using script.echo"
-    script.echo "number of Doors ${numberOfDoors}"
-    script.echo "model ${model}"
-  }
+    Car(script) {
+      this.script = script
+    }
+
+    void printOutCar() {
+      script.echo "using script.echo"
+      script.echo "number of Doors ${numberOfDoors}"
+      script.echo "model ${model}"
+    }
 }
